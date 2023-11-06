0b0a0d672cbd1e8d4ad346a2c76af4b842172e4c
zharkoexx <sushshiit@gmail.com>
2023-11-06 11:54:46 +0600

    add

---
+++ video.js
@@ -0,0 +1,23 @@
+    // Get the video element:
+    const video = document.getElementById("myVideo");
+    
+    // Initialize a variable to track the current playback speed
+    let currentSpeed = 1;
+    
+    // Function to toggle between normal and slow motion
+    function togglePlaySpeed() { 
+      if (currentSpeed === 1) {
+        // Change to slow motion (0.3x speed)
+        video.playbackRate = 0.3;
+        currentSpeed = 0.3;
+      } else {
+        // Change to normal speed (1x speed)
+        video.playbackRate = 1;
+        currentSpeed = 1;
+      }
+    } 
+    
+    // Alert some text when the playing speed of the video is changed
+    function myFunction() { 
+      document.getElementById("demo").innerHTML = "The playing speed of the video was changed.";
+    }
